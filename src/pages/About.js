edc1a41fdeeac2e38c0d78158f9b69d4e0a8e90a
Zoube <zoubeyamadou1@gmail.com>
2022-07-10 23:09:54 +0000

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import Logo from '../components/Logo'
+import Navigation from '../components/Navigation'
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+        <h1> A PROPOS</h1>
+        <p> 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+           Assumenda veniam corrupti et libero inventore maxime mollitia harum, 
+           pariatur blanditiis quae iste ex deleniti expedita excepturi dicta eius maiores dolore adipisci eos.
+            Eligendi quo vel sequi! Nihil voluptates cumque, dolorem, iusto rem pariatur sit officiis,
+            dicta magnam error distinctio natus voluptatem sequi ab totam doloremque voluptatibus ratione est. 
+            Molestiae vitae nihil aut iure omnis nam officia, deserunt eveniet culpa eligendi inventore est, 
+            optio neque quibusdam adipisci quo possimus incidunt perspiciatis quae voluptate. Facilis, 
+            saepe officia laboriosam labore voluptate cupiditate dolor nam ab, voluptatum error officiis porro
+             molestias dicta soluta accusamus. 
+        </p>
+        <br/>
+        <p>
+        Nihil quaerat qui tempora odio eaque, dolorem neque consectetur 
+             voluptatem facilis illum modi amet ullam corrupti temporibus? Perspiciatis consequuntur nesciunt 
+             distinctio nemo repellat natus adipisci similique corporis fugit cumque a officiis, fuga, saepe 
+             assumenda accusamus esse sequi ratione. Dolores obcaecati tenetur soluta magni optio, quisquam 
+             error molestias? Fugiat, temporibus et! Natus, magni! Vitae expedita necessitatibus amet ea 
+             repellendus distinctio tempore maxime nisi rem aspernatur perspiciatis incidunt tempora libero
+              voluptate, deserunt, consequuntur temporibus ullam asperiores a. Inventore pariatur esse nam 
+              adipisci eius, dignissimos veritatis id architecto. Architecto accusamus hic nesciunt officia
+               ex perferendis nobis placeat, maiores omnis qui expedita praesentium ab asperiores cumque 
+               voluptate voluptates possimus deserunt repellat, assumenda iusto minus vero fuga nam.
+                Molestiae atque nisi officiis blanditiis velit necessitatibus. Non, provident ducimus alias
+                molestiae nemo itaque libero pariatur voluptates eius eligendi eaque ea accusantium iusto,
+                 ut exercitationem ratione magnam numquam, magni eveniet perferendis architecto praesentium! 
+                 Id quibusdam corporis dolorem iusto perspiciatis aspernatur molestias repellendus saepe! 
+                 Voluptatum laudantium nesciunt quam odit commodi! Placeat iste optio quidem libero dolor at voluptatem, 
+                 ipsam possimus praesentium quam quibusdam maiores architecto unde, quisquam animi similique sequi 
+                 voluptate velit ullam neque? Maxime adipisci odio voluptatum atque, laudantium ullam commodi at a,
+                  explicabo quas, fugiat blanditiis nisi veritatis fugit molestias tempore totam. Atque sit cumque
+                   ipsa corrupti magnam. Vero error voluptas reiciendis deserunt deleniti quae, quo corrupti facere debitis
+                   . Accusamus optio eius obcaecati recusandae quaerat quibusdam nihil, laboriosam inventore dolor unde
+                    cumque. Quas, quasi? Sunt sequi nostrum a sapiente repellendus consequatur autem delectus doloremque
+                     voluptatibus reiciendis quis debitis labore pariatur quas odit architecto temporibus, culpa eos modi,
+                     similique, maiores nihil. Corporis quas, iusto deleniti dicta qui est magni eius nobis ullam, nesciunt
+                      dolorum dolorem excepturi voluptatem esse aut beatae. Esse iusto pariatur iure
+           mollitia cumque dolore ea, ipsam aperiam, architecto qui vero molestiae maiores, asperiores eos 
+           dolorum!
+
+        </p>
+        </div>
+  )
+}
+
+export default About
